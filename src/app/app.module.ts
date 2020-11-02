355ada359f03879d6bfe1403c91202d17d17c3bc
ali <aliqbhastam@gmail.com>
2020-11-02 19:11:53 +0330

    make components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileDetailComponent } from './profile/profile-detail/profile-detail.component';
+import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostComponent } from './share/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AuthComponent,
+    ProfileComponent,
+    ProfileDetailComponent,
+    ProfileEditComponent,
+    PostDetailComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
