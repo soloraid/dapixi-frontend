f51743d3033289bcae16f1989e1b239071ebb7c9
ali <aliqbhastam@gmail.com>
2020-11-05 19:06:18 +0330

    make notif text interactive

--- src/app/profile/profile-detail/profile-detail.component.ts
+++ src/app/profile/profile-detail/profile-detail.component.ts
@@ -14,14 +14,15 @@ export class ProfileDetailComponent implements OnInit {
     username:"@a",
     email:"a@a.com"
   }
+  copied:boolean=false;
   link:string;
   constructor() { }
 
   ngOnInit(): void {
     this.link=window.location.href;
   }
-  // copyLink(){
-  //   console.log(window.location.href);
-  // }
+  copyLink(){
+    this.copied=true;
+  }
 
 }
