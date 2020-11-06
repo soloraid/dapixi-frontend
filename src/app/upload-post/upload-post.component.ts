035a66d95adeab1f66d7a98fad530bea2475e7ab
ali <aliqbhastam@gmail.com>
2020-11-06 21:10:36 +0330

    basic tasks for upload and making interact

--- src/app/upload-post/upload-post.component.ts
+++ src/app/upload-post/upload-post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-upload-post',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-post.component.scss']
 })
 export class UploadPostComponent implements OnInit {
-
+  imgFile:File;
+  description:string;
+  @ViewChild('file_label') label:ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+    // console.log(this.label);
+  }
+  onChange(event){
+    console.log(event.target.files[0]);
+    this.imgFile=event.target.files[0];
+    // console.log(this.label);
+    this.label.nativeElement.innerHTML=this.imgFile.name;
+
+    
+  }
+  onSubmit(){
+    console.log(this.imgFile,this.description);
   }
 
 }
