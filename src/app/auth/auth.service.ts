d6b977a4d8c2e9dfff19d8dd108025297ef88560
ali <aliqbhastam@gmail.com>
2020-11-12 19:26:58 +0330

    add login method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  bsicToken:"ZGFwaXhpOnRoaXNpc3NlY3JldA==";
+  constructor(private _http:HttpClient) {
+   }
+   login(){
+    this._http.post(
+      environment.api+"/auth/oauth/token",
+      {
+        grant_type:"password",
+        scope:"webclient",
+        username:"shayan",
+        password:"password"
 
-  constructor() { }
+      },
+      {
+        headers:new HttpHeaders({
+          Authorization:"Basic "+this.bsicToken
+        })
+      }
+      ).subscribe(data=>{
+        console.log(data);
+      })
+   }
 }
