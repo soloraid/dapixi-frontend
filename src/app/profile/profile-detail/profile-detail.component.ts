e147ed10306747e1b3f6484af36e24e659498041
ali <aliqbhastam@gmail.com>
2020-11-12 19:53:50 +0330

    test

--- src/app/profile/profile-detail/profile-detail.component.ts
+++ src/app/profile/profile-detail/profile-detail.component.ts
@@ -1,6 +1,8 @@
 import { Location } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from "../../../environments/environment.prod"
 
 @Component({
   selector: 'app-profile-detail',
@@ -8,24 +10,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile-detail.component.scss']
 })
 export class ProfileDetailComponent implements OnInit {
-  userView={
-    img:"https://via.placeholder.com/150",
-    name:"علی قیومی",
-    username:"@a",
-    email:"a@a.com"
+  userView = {
+    img: "https://via.placeholder.com/150",
+    name: "علی قیومی",
+    username: "@a",
+    email: "a@a.com"
   }
-  copied:boolean=false;
-  link:string;
-  constructor() { }
+  copied: boolean = false;
+  link: string;
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
-    this.link=window.location.href;
+    this.link = window.location.href;
+    // this.http.get(environment.api + "/auth/user/1", {
+    //   headers: new HttpHeaders({
+    //     Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDUyNDA3ODYsInVzZXJfbmFtZSI6InNoYXlhbiIsImF1dGhvcml0aWVzIjpbIlVTRVIiLCJBRE1JTiJdLCJqdGkiOiIyNTM4ZDBkOC0yZTQ4LTQ4MTgtYWI0Yy1jMTIxMDc0MWRmMWIiLCJjbGllbnRfaWQiOiJkYXBpeGkiLCJzY29wZSI6WyJ3ZWJjbGllbnQiXX0.n1JaNIri4KzScGOsCq2n1INOdrubk88BfHXmpkdOits"
+    //   })
+    // }).subscribe(data=>{
+    //   console.log(data)
+    // })
   }
-  copyLink(){
-    this.copied=true;
-    setTimeout(()=>{
-      this.copied=false;
-    },3000);
+  copyLink() {
+    this.copied = true;
+    setTimeout(() => {
+      this.copied = false;
+    }, 3000);
   }
 
 }
