0c170230d8b1ff9e602b1738af5110f0b600f2a8
ali <aliqbhastam@gmail.com>
2020-11-15 22:39:00 +0330

    handle date validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -17,7 +17,7 @@ export class RegisterComponent implements OnInit {
       'lastName':new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(32)]),
       'userName':new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(32)]),
       'phone':new FormControl('',Validators.pattern(/(09\d{9})|([+|0]989\d{9})/)),
-      'date':new FormControl('',Validators.required),
+      'date':new FormControl('',[Validators.required,this.dateValidator.bind(this)]),
       'email':new FormControl('',[Validators.required,Validators.email]),
       'password':new FormControl('',[Validators.required,Validators.minLength(6)]),
       'passwordRep':new FormControl('',[Validators.required,Validators.minLength(6)])
@@ -25,10 +25,9 @@ export class RegisterComponent implements OnInit {
     })
   }
   onSubmit(){
-    console.log(this.regForm.controls['phone']);
-    // this._authService.signIn();
+    console.log(this.regForm)
   }
-  minError(formControlName:string){
+   minError(formControlName:string){
     // console.log('v',this.regForm.controls[formControlName].errors);
     if(this.regForm.controls[formControlName].errors && this.regForm.controls[formControlName].errors['minlength']){
       if(this.regForm.controls[formControlName].errors['minlength'] && this.regForm.controls[formControlName].touched){
@@ -36,6 +35,16 @@ export class RegisterComponent implements OnInit {
       }else{
         return false;
       }
+    }
+  }
+  private dateValidator(formControl:FormControl):{[k:string]:boolean}|null{
+    const now=new Date();
+    const today=new Date(now.getFullYear(),now.getMonth(),now.getDate());
+    const input=new Date(Date.parse(formControl.value+"T00:00:00.000"));
+    if(input.getTime()>=today.getTime()){
+      return {'today':true}
+    }else{
+      return null;
     }
   }
 }
