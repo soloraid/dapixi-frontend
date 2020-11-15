b40650c8f8bb5a490fd0648f94d754486603e79a
ali <aliqbhastam@gmail.com>
2020-11-15 19:36:04 +0330

    switch td form to ra

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -8,12 +8,24 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  regForm:FormGroup;
   constructor(private _authService:AuthService) { }
 
   ngOnInit(): void {
+    this.regForm=new FormGroup({
+      'firstName':new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(32)]),
+      'lastName':new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(32)]),
+      'userName':new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(32)]),
+      'phone':new FormControl('',Validators.pattern(/(09\d{9})|([+|0]989\d{9})/)),
+      'date':new FormControl('',Validators.required),
+      'email':new FormControl('',[Validators.required,Validators.email]),
+      'password':new FormControl('',[Validators.required,Validators.minLength(6)]),
+      'passwordRep':new FormControl('',[Validators.required,Validators.minLength(6)])
+      
+    })
   }
-  onSubmit(form:NgForm){
-    // console.log(form);
-    this._authService.signIn();
+  onSubmit(){
+    console.log(this.regForm);
+    // this._authService.signIn();
   }
 }
