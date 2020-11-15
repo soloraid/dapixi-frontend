dc35fc0458d9d6dcccac09e85862e48f3ad76b74
ali <aliqbhastam@gmail.com>
2020-11-15 18:29:05 +0330

    react home with auth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { tokens } from '../share/tokens.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit,OnDestroy {
+  isAuth:boolean;
+  authSubsc:Subscription
+  constructor(private _authService:AuthService) { }
 
   ngOnInit(): void {
+    this.authSubsc=this._authService.authState.subscribe((token:tokens)=>{
+      this.isAuth= !!token;
+    })
+  }
+  ngOnDestroy(){
+    this.authSubsc.unsubscribe();
   }
 
 }
