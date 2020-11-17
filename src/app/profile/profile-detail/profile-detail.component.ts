0cfdb80d6c187c3414ac80db1f8bc9538c487347
ali <aliqbhastam@gmail.com>
2020-11-17 15:34:36 +0330

    add profile by id

--- src/app/profile/profile-detail/profile-detail.component.ts
+++ src/app/profile/profile-detail/profile-detail.component.ts
@@ -1,7 +1,7 @@
 import { Location } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from "../../../environments/environment.prod"
 import { ProfileService } from '../profile.service';
 
@@ -11,7 +11,9 @@ import { ProfileService } from '../profile.service';
   styleUrls: ['./profile-detail.component.scss']
 })
 export class ProfileDetailComponent implements OnInit {
-  userView:User
+  userView: User;
+  id: string;
+  currnetUser: boolean = true;
   // = {
   //   img: "https://via.placeholder.com/150",
   //   name: "علی قیومی",
@@ -20,13 +22,29 @@ export class ProfileDetailComponent implements OnInit {
   // }
   copied: boolean = false;
   link: string;
-  constructor(public http: HttpClient,private _profile:ProfileService) { }
+  constructor(public http: HttpClient, private _profile: ProfileService, private _rout: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.link = window.location.href;
-    this._profile.getProfile().subscribe((user:User)=>{
-      this.userView=user;
+    if(this.currnetUser){
+      const index=this.link.indexOf('/profile');
+      this.link=this.link.slice(0,index);
+    }
+    this._rout.params.subscribe(() => {
+      this.id = this._rout.snapshot.params['id'];
+      if (this.id) {
+        this.currnetUser = false;
+      }
     })
+    if (this.currnetUser) {
+      this._profile.getProfile().subscribe((user: User) => {
+        this.userView = user;
+      })
+    }else{
+      this._profile.getProfileById(this.id).subscribe((user:User)=>{
+        this.userView=user;
+      })
+    }
     // this.http.get(environment.api + "/auth/user/1", {
     //   headers: new HttpHeaders({
     //     Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDUyNDA3ODYsInVzZXJfbmFtZSI6InNoYXlhbiIsImF1dGhvcml0aWVzIjpbIlVTRVIiLCJBRE1JTiJdLCJqdGkiOiIyNTM4ZDBkOC0yZTQ4LTQ4MTgtYWI0Yy1jMTIxMDc0MWRmMWIiLCJjbGllbnRfaWQiOiJkYXBpeGkiLCJzY29wZSI6WyJ3ZWJjbGllbnQiXX0.n1JaNIri4KzScGOsCq2n1INOdrubk88BfHXmpkdOits"
@@ -43,8 +61,8 @@ export class ProfileDetailComponent implements OnInit {
   }
 
 }
-interface User{
-  username:string,
+interface User {
+  username: string,
   firstName: string,
   lastName: string,
   mobile?: string,
