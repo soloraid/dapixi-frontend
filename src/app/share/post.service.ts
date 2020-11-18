b02df102db8c92d12bf4f40f26ff3bfd9ddbded1
hashem <hm.praw@yahoo.com>
2020-11-18 20:55:48 +0330

    create postService for http request method

---
+++ src/app/share/post.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private http: HttpClient) { }
+
+  getLatestPost() {
+     return this.http.get(environment.api + '/photo/latest/posts');
+  }
+}
+
+
