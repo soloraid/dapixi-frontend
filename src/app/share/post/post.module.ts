c9b9098f7ac9d1f02dd4be269263ef1022aab686
hashem <hm.praw@yahoo.com>
2020-11-18 20:56:16 +0330

    create class post for interact api

---
+++ src/app/share/post/post.module.ts
@@ -0,0 +1,12 @@
+export class Post {
+  id: string;
+  username: string;
+  title: string;
+  description: string;
+  creationDate: string;
+  imageUrl: string;
+  userRating: 0;
+  averageRating: 0;
+  categories: string[];
+
+}
