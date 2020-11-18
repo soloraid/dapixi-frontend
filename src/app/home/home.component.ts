af83a77ccad5bb4e8e91e882b106b24fc81d77e9
hashem <hm.praw@yahoo.com>
2020-11-18 20:58:26 +0330

    use postService to get httpRequest

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,40 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AuthService } from '../auth/auth.service';
-import { Tokens } from '../share/tokens.model';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {AuthService} from '../auth/auth.service';
+import {Tokens} from '../share/tokens.model';
+import {Post} from '../share/post/post.module';
+import {PostService} from '../share/post.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit,OnDestroy {
-  isAuth:boolean;
-  authSubsc:Subscription
-  constructor(private _authService:AuthService) { }
+export class HomeComponent implements OnInit, OnDestroy {
+  isAuth: boolean;
+  authSubsc: Subscription;
+  postView: Post[] = [];
+  isEmpty = false;
+
+  // tslint:disable-next-line:variable-name
+  constructor(private _authService: AuthService, private postService: PostService) {
+  }
 
   ngOnInit(): void {
-    this.authSubsc=this._authService.authState.subscribe((token:Tokens)=>{
-      this.isAuth= !!token;
-    })
+    this.authSubsc = this._authService.authState.subscribe((token: Tokens) => {
+      this.isAuth = !!token;
+    });
+    this.postService.getLatestPost().subscribe(posts => {
+      console.log(posts);
+      // tslint:disable-next-line:forin
+      for (let index in posts) {
+        this.postView.push(posts[index]);
+      }
+      this.isEmpty = true;
+    });
   }
-  ngOnDestroy(){
+
+  ngOnDestroy() {
     this.authSubsc.unsubscribe();
   }
 
