6e08d6e3bddc265f6f0927f1860bc699ea68ea1b
hashem <hm.praw@yahoo.com>
2020-11-19 02:40:40 +0330

    searchBar component created

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  term: string;
+  hasValue: boolean;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
