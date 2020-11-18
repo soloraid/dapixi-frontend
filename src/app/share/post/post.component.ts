e88a1096507cb722f7c278ae8039a39d56354825
hashem <hm.praw@yahoo.com>
2020-11-18 20:57:07 +0330

    create field post for binding in UI

--- src/app/share/post/post.component.ts
+++ src/app/share/post/post.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from './post.module';
+import {environment} from '../../../environments/environment.prod';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
+  // tslint:disable-next-line:no-input-rename
+  @Input() post: Post;
+  postUrl: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.postUrl = environment.api + '/photo' + this.post.imageUrl;
   }
 
 }
