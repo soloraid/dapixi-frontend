0724b4bf375b8ccc0893ce75fae34bf03d6330d3
hashem <hm.praw@yahoo.com>
2020-11-19 16:52:38 +0330

    search result component created for ui

---
+++ src/app/search/search-result/search-result.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {SearchService} from '../search.service';
+import {ActivatedRoute} from '@angular/router';
+import {Post} from '../../share/post/post.module';
+import {Subject} from 'rxjs';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.scss']
+})
+export class SearchResultComponent implements OnInit {
+  posts: Post[] = [];
+  isEmptyPosts = true;
+  p = 1;
+
+  constructor(private searchService: SearchService) { }
+
+
+  ngOnInit(): void {
+    this.searchService.resultSubject.subscribe(posts => {
+      this.posts = posts;
+      if (posts.length !== 0) {
+        console.log(posts);
+        this.isEmptyPosts = false;
+      } else {
+        this.isEmptyPosts = true;
+      }
+    });
+  }
+}
