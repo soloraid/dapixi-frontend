48883739f1b4a1fb734fdf4340e4f306b60b14b6
hashem <hm.praw@yahoo.com>
2020-11-19 16:56:33 +0330

    inject searchService to handle then resultposts of searching and method added.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {SearchService} from './search.service';
+import {Post} from '../share/post/post.module';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -6,13 +9,29 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  term: string;
-  hasValue: boolean;
+  searchUser: string;
+  arrPosts: Post[] = [];
 
-  constructor() {
+
+  constructor(private searchService: SearchService, private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
+  onSearch(input: string): void {
+    this.searchUser = input;
+    // console.log(this.searchUser);
+    this.searchService.searchData(this.searchUser).subscribe(posts => {
+
+      // tslint:disable-next-line:forin
+      for (let index in posts) {
+        this.arrPosts.push(posts[index]);
+      }
+      this.searchService.setResult(this.arrPosts);
+    });
+    this.router.navigate(['/search/result']);
+    this.searchUser = '';
+    this.arrPosts = [];
+  }
 }
