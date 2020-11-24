81dcf34179758d08f06d59d9ea70c7b4d0dfe8ab
ali <aliqbhastam@gmail.com>
2020-11-24 22:19:10 +0330

    finish remmember me functinality

--- src/app/auth/enter/enter.component.ts
+++ src/app/auth/enter/enter.component.ts
@@ -1,5 +1,5 @@
 import { NgForOf } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -14,32 +14,57 @@ export class EnterComponent implements OnInit,OnDestroy {
 
   constructor(private _authService: AuthService,private _router:Router) { }
   errorMsg: string = "";
+  @ViewChild('enter_form',{static:false}) enterForm:NgForm;
+  username:string;
+  password:String;
+  hasRemember:boolean=false;
   loginSubs:Subscription;
   ngOnInit(): void {
+    const username=localStorage.getItem('username');
+    const password=localStorage.getItem('password');
+    if(username && password){
+      this.username=username;
+      this.password=password;
+      this.hasRemember=true;
+    }
+    // console.log(this.enterForm);
+
   }
-  onSubmit(form: NgForm) {
-    console.log(form);
-    console.log(!!form.value['remember']);
-    // this.loginSubs=this._authService
-    //   .login(form.value.username, form.value.password)
-    //   .subscribe(
-    //      data => {
-    //       console.log(data);
-    //       this._router.navigate(['/'])
-    //     },
-    //     (errorData: string) => {
-    //       console.log(errorData);
-    //       this.errorMsg = errorData;
-    //       setTimeout(()=>{
-    //         this.errorMsg="";
-    //       },3000)
-    //     }
-    //   )
+  onSubmit() {
+    // console.log(this.form);
+    // console.log(!!form.value['remember']);
+    const username=this.enterForm.value.username;
+    const password=this.enterForm.value.password;
+    this.loginSubs=this._authService
+      .login(username,password)
+      .subscribe(
+         data => {
+          console.log(data);
+          if(this.enterForm.value['remember']){
+            this.remember(username,password);
+          }else{
+            localStorage.removeItem('username');
+            localStorage.removeItem('password');
+          }
+          this._router.navigate(['/']);
+        },
+        (errorData: string) => {
+          console.log(errorData);
+          this.errorMsg = errorData;
+          setTimeout(()=>{
+            this.errorMsg="";
+          },3000)
+        }
+      )
   }
   ngOnDestroy(){
     if(this.loginSubs){
 
       this.loginSubs.unsubscribe();
     }
+  }
+  private remember(username:string,password:string){
+    localStorage.setItem('username',username);
+    localStorage.setItem('password',password);
   }
 }
