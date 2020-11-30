b57c19e3b8ff048605061d55a9b100b35ff8e413
hashem <hm.praw@yahoo.com>
2020-11-30 20:41:24 +0330

    component main-nav in material design added

---
+++ src/app/header/main-nav/main-nav.component.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable, Subscription} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+import {AuthService} from '../../auth/auth.service';
+import {Tokens} from '../../share/tokens.model';
+
+@Component({
+  selector: 'app-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.scss',]
+})
+export class MainNavComponent {
+  isAuth: boolean;
+  authSubsc: Subscription;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  // tslint:disable-next-line:variable-name
+  constructor(private breakpointObserver: BreakpointObserver, private _authService: AuthService) {
+  }
+
+
+
+  ngOnInit(): void {
+    this.authSubsc = this._authService.authState.subscribe((token: Tokens) => {
+      this.isAuth = !!token;
+    });
+  }
+
+  onLogOut() {
+    this._authService.logOut();
+  }
+
+  ngOnDestroy() {
+    this.authSubsc.unsubscribe();
+  }
+}
