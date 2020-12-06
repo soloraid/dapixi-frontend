21b16410178e15f8f40010fc30b12d0dfc0157c4
ali <aliqbhastam@gmail.com>
2020-12-06 19:25:44 +0330

    make component add rout from latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
-import {registerLocaleData} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {AuthComponent} from './auth/auth.component';
-import {EnterComponent} from './auth/enter/enter.component';
-import {RegisterComponent} from './auth/register/register.component';
-import {HomeComponent} from './home/home.component';
-import {ProfileDetailComponent} from './profile/profile-detail/profile-detail.component';
-import {ProfileEditComponent} from './profile/profile-edit/profile-edit.component';
-import {ProfileComponent} from './profile/profile.component';
-import {UploadPostComponent} from './upload-post/upload-post.component';
-import {PostDetailComponent} from './post-detail/post-detail.component';
-import {SearchResultComponent} from './search/search-result/search-result.component';
+import { registerLocaleData } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { EnterComponent } from './auth/enter/enter.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileDetailComponent } from './profile/profile-detail/profile-detail.component';
+import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UploadPostComponent } from './upload-post/upload-post.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { SearchResultComponent } from './search/search-result/search-result.component';
 import { AuthGuard } from './share/auth.guard';
 import { ReAuthGuard } from './share/re-auth.guard';
 import { ResetComponent } from './auth/reset/reset.component';
 import { Error404Component } from './error404/error404.component';
-import {Error500Component} from './error500/error500.component';
+import { Error500Component } from './error500/error500.component';
+import { MoreComponent } from './more/more.component';
 
 
 const routes: Routes = [
@@ -29,6 +30,10 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
+    path: 'more/:type',
+    component: MoreComponent
+  },
+  {
     path: 'post-detail/:id',
     component: PostDetailComponent
   },
@@ -39,7 +44,7 @@ const routes: Routes = [
   {
     path: 'auth',
     component: AuthComponent,
-    canActivate:[ReAuthGuard],
+    canActivate: [ReAuthGuard],
     children: [
       {
         path: '',
@@ -57,7 +62,7 @@ const routes: Routes = [
       },
       {
         path: 'reset',
-        component:ResetComponent
+        component: ResetComponent
       }
     ]
   },
@@ -70,12 +75,12 @@ const routes: Routes = [
         path: '',
         redirectTo: 'profile',
         pathMatch: 'full',
-        canActivate:[AuthGuard]
+        canActivate: [AuthGuard]
       },
       {
         path: 'profile',
         component: ProfileDetailComponent,
-        canActivate:[AuthGuard]
+        canActivate: [AuthGuard]
         // children:[
         //   {
         //     path:'edit',
@@ -86,12 +91,12 @@ const routes: Routes = [
       {
         path: 'profile/edit',
         component: ProfileEditComponent,
-        canActivate:[AuthGuard]
+        canActivate: [AuthGuard]
       },
       {
         path: 'new',
         component: UploadPostComponent,
-        canActivate:[AuthGuard]
+        canActivate: [AuthGuard]
       },
       {
         path: ':username',
@@ -100,16 +105,16 @@ const routes: Routes = [
     ]
   },
   {
-    path:'404',
-    component:Error404Component
+    path: '404',
+    component: Error404Component
   },
-  /*{
-    path:'**',
-    component:Error404Component
-  },*/
   {
     path: '500',
     component: Error500Component
+  },
+  {
+    path: '**',
+    component: Error404Component
   }
 ];
 
