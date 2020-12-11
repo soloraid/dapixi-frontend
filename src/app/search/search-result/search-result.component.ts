81ceef6261d862e30a9425c03d2ff0a99f0248f1
hashem <hm.praw@yahoo.com>
2020-12-11 05:33:48 +0330

    requests rearrange

--- src/app/search/search-result/search-result.component.ts
+++ src/app/search/search-result/search-result.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {Post} from '../../share/post/post.module';
 import {ActivatedRoute} from '@angular/router';
 import {PostService} from '../../share/post.service';
+import {User} from '../../share/user/user.mudole';
+import {ProfileService} from '../../profile/profile.service';
+import {environment} from '../../../environments/environment.prod';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-search-result',
@@ -9,30 +12,43 @@ import {PostService} from '../../share/post.service';
   styleUrls: ['./search-result.component.scss']
 })
 export class SearchResultComponent implements OnInit {
-  posts: Post[];
+  users: User[];
   userName: string;
-  isEmptyPosts = true;
+  isEmptyUsers = true;
   p = 1;
 
-  constructor(private postService: PostService, private route: ActivatedRoute) {
+
+  constructor(private postService: PostService, private route: ActivatedRoute, private profileService: ProfileService) {
   }
 
 
   ngOnInit(): void {
 
-  //   this.route.params.subscribe((params) => {
-  //     this.userName = params.name;
-  //     this.postService.getPostByUserName(this.userName).subscribe((posts: Post[]) => {
-  //       this.posts = posts;
-  //       if (posts.length !== 0) {
-  //         this.isEmptyPosts = false;
-  //       } else {
-  //         this.isEmptyPosts = true;
-  //       }
-  //     });
-  //   });
+    this.route.params.subscribe((params) => {
+      this.userName = params.name;
+      this.postService.getProfileByUserName(this.userName).subscribe((users: User[]) => {
+        // tslint:disable-next-line:forin
+        for (const index in users) {
+          this.profileService.getProfilePic(users[index].username).subscribe((picData: PictureData) => {
+              users[index].profileImageUrl = environment.api + '/photo/' + picData.imageUrl;
+            },
+            (errorData: HttpErrorResponse) => {
+              users[index].profileImageUrl = '../../../assets/avatar-default.png';
+            });
+        }
+        this.users = users;
+        if (users.length !== 0) {
+          this.isEmptyUsers = false;
+        } else {
+          this.isEmptyUsers = true;
+        }
+      });
+    });
 
-   }
-
+  }
 }
 
+interface PictureData {
+  username: string;
+  imageUrl: string;
+}
