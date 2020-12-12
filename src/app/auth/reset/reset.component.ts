1d80bb6ee9d2b9cdcb0edafda43c98fd7f9c555a
ali <aliqbhastam@gmail.com>
2020-12-13 00:56:46 +0330

    arrange template

--- src/app/auth/reset/reset.component.ts
+++ src/app/auth/reset/reset.component.ts
@@ -10,38 +10,16 @@ import {LoaderService} from '../../share/loader/loader.service';
 export class ResetComponent implements OnInit {
   resetForm:FormGroup;
   passwordHolder:string='';
+  confirm:boolean=false;
   constructor(public loaderService: LoaderService) { }
 
   ngOnInit(): void {
     this.resetForm=new FormGroup({
-      'password':new FormControl('',[Validators.required,Validators.minLength(6)]),
-      'passwordRep':new FormControl('',[Validators.required,Validators.minLength(6),this.passwordRepeatValidator.bind(this)])
+      'email':new FormControl('',[Validators.required,Validators.email])
     })
-  }
-  onPasswordChange(event){
-    this.passwordHolder=event.target.value;
-    // console.log(event.target.value);
-    // console.log(this.passwordHolder);
-  }
-  minError(formControlName:string){
-    // console.log('v',this.resetForm.controls[formControlName].errors);
-    if(this.resetForm.controls[formControlName].errors && this.resetForm.controls[formControlName].errors['minlength']){
-      if(this.resetForm.controls[formControlName].errors['minlength'] && this.resetForm.controls[formControlName].touched){
-        return true;
-      }else{
-        return false;
-      }
-    }
   }
   onSubmit(){
     console.log(this.resetForm);
-  }
-  private passwordRepeatValidator(formControl:FormControl):{[k:string]:boolean}|null{
-    if(formControl.value!==this.passwordHolder){
-      return {'repeat':true};
-    }else{
-      return null
-    }
   }
 
 }
