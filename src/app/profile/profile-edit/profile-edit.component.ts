daa0b5b414e27cda90d269caddfafd3795a93274
ali <aliqbhastam@gmail.com>
2020-12-13 14:57:03 +0330

    bsic changes

--- src/app/profile/profile-edit/profile-edit.component.ts
+++ src/app/profile/profile-edit/profile-edit.component.ts
@@ -1,16 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-import {Subscription} from 'rxjs';
-import {ProfileService} from '../profile.service';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { Subscription } from 'rxjs';
+import { ProfileService } from '../profile.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.component.html',
   styleUrls: ['./profile-edit.component.scss'],
   providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: { displayDefaultIndicatorType: false }
   }]
 })
 export class ProfileEditComponent implements OnInit {
@@ -19,18 +19,24 @@ export class ProfileEditComponent implements OnInit {
   phoneForm: FormGroup;
   dateForm: FormGroup;
   passwordForm: FormGroup;
+  firstName: string;
+  lastname: string;
+  email:string;
+  phone:string;
+  date:string;
+  password:string;
   hide1 = true;
   hide2 = true;
   passwordHolder = '';
   errorMsg = '';
   editSubs: Subscription;
-  isChangeName = true;
-  isChangeEmail = true;
-  isChangePhone = true;
-  isChangeDate = true;
-  isChangePassword = true;
+  isChangeName = false;
+  isChangeEmail = false;
+  isChangePhone = false;
+  isChangeDate = false;
+  isChangePassword = false;
 
-  constructor(private profileService: ProfileService, private router: Router){
+  constructor(private profileService: ProfileService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -56,24 +62,24 @@ export class ProfileEditComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.isChangeName) {
-      this.profileService.editProfileFirstLastName(
-        this.firstLastNameForm.get('firstName').value,
-        this.firstLastNameForm.get('lastName').value
-      );
-    }
-    if (this.isChangeEmail) {
-      this.profileService.editProfileEmail(this.emailForm.get('email').value);
-    }
-    if (this.isChangePhone) {
-      this.profileService.editProfilePhone(this.phoneForm.get('phone').value);
-    }
-    if (this.isChangeDate) {
-      this.profileService.editProfileDate(this.dateForm.get('date').value);
-    }
-    if (this.isChangePassword) {
-      this.profileService.editProfilePassword(this.passwordForm.get('password').value);
-    }
+    // if (this.isChangeName) {
+    //   this.profileService.editProfileFirstLastName(
+    //     this.firstLastNameForm.get('firstName').value,
+    //     this.firstLastNameForm.get('lastName').value
+    //   );
+    // }
+    // if (this.isChangeEmail) {
+    //   this.profileService.editProfileEmail(this.emailForm.get('email').value);
+    // }
+    // if (this.isChangePhone) {
+    //   this.profileService.editProfilePhone(this.phoneForm.get('phone').value);
+    // }
+    // if (this.isChangeDate) {
+    //   this.profileService.editProfileDate(this.dateForm.get('date').value);
+    // }
+    // if (this.isChangePassword) {
+    //   this.profileService.editProfilePassword(this.passwordForm.get('password').value);
+    // }
   }
 
   private dateValidator(formControl: FormControl): { [k: string]: boolean } | null {
@@ -81,7 +87,7 @@ export class ProfileEditComponent implements OnInit {
     const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
     const input = new Date(Date.parse(formControl.value + 'T00:00:00.000'));
     if (input.getTime() >= today.getTime()) {
-      return {today: true};
+      return { today: true };
     } else {
       return null;
     }
@@ -89,27 +95,45 @@ export class ProfileEditComponent implements OnInit {
 
   private passwordRepeatValidator(formControl: FormControl): { [k: string]: boolean } | null {
     if (formControl.value !== this.passwordHolder) {
-      return {repeat: true};
+      return { repeat: true };
     } else {
       return null;
     }
   }
 
   changeName(): void {
-    this.isChangeName = false;
+    this.firstName = this.firstLastNameForm.get('firstName').value;
+    this.lastname = this.firstLastNameForm.get('lastName').value;
+    if(this.lastname && this.firstName){
+      this.isChangeName=true;
+    }
+    console.log(this.isChangeName);
+    // console.log(this.firstName,'\n',this.lastname);
   }
 
   changeEmail(): void {
-    this.isChangeEmail = false;
+    this.email=this.emailForm.get('email').value;
+    if(this.email){
+      this.isChangeEmail=true;
+    }
   }
 
   changePhone(): void {
-    this.isChangePhone = false;
+    this.phone=this.phoneForm.get('phone').value;
+    if(this.phone){
+      this.isChangePhone=true;
+    }
   }
-  changeDate(): void  {
-    this.isChangeDate = false;
+  changeDate(): void {
+    this.date=this.dateForm.get('date').value;
+    if(this.date){
+      this.isChangeDate=true;
+    }
   }
-  changePassword(): void  {
-    this.isChangePassword = false;
+  changePassword(): void {
+    this.password=this.passwordForm.get('password').value;
+    if(this.password){
+      this.isChangePassword=true;
+    }
   }
 }
