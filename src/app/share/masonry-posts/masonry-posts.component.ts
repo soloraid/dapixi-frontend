2ab227680270b4ea20218c53b81fa743e7292607
ali <aliqbhastam@gmail.com>
2020-12-23 23:12:10 +0330

    add more btn

--- src/app/share/masonry-posts/masonry-posts.component.ts
+++ src/app/share/masonry-posts/masonry-posts.component.ts
@@ -11,22 +11,11 @@ import { Post } from '../post/post.module';
 export class MasonryPostsComponent implements OnInit {
   @Input() posts:Post[]=[];
   isExist:boolean;
-  @Input() getFunction:(Function);
+  @Input() mode:string;
   subs:Subscription
   constructor(private _postService:PostService) { }
 
   ngOnInit(): void {
-    // this.subs=(<Observable<any>>this.getFunction(9)).subscribe((posts:Post[])=>{
-    //   console.log(posts)
-    //   if(this.posts.length>0){
-    //     posts=posts;
-    //   }
-    // })
-    // this.subs=this._postService['getLatestPost'](9).subscribe((posts:Post[])=>{
-    //   if(posts.length){
-    //     this.posts=posts;
-    //   }
-    // })
   }
 
 }
