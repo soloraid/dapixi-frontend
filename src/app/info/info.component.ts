a715bc932e471355f2e8c6a59f8b441bdaeafcba
ali <aliqbhastam@gmail.com>
2021-01-03 19:22:24 +0330

    bind form

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-info',
@@ -10,6 +11,9 @@ export class InfoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit(form:NgForm){
+    console.log(form.value.title,form.value.message);
   }
 
 }
