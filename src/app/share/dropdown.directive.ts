2d6f818ca4c9541353d4490f32db60fe30f16b02
ali <aliqbhastam@gmail.com>
2021-01-04 13:34:21 +0330

    clean share

--- src/app/share/dropdown.directive.ts
+++ src/app/share/dropdown.directive.ts
@@ -6,10 +6,8 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class DropdownDirective {
 
   @HostListener('document:click', ['$event']) toggling(event: Event) {
-    // console.log((<HTMLElement>event.target).classList.contains('dropdown-toggle'));
     const targetElement: HTMLElement = (<HTMLElement>event.target);
     const menu: HTMLElement = this.element.nativeElement.nextElementSibling;
-    // console.log(targetElement,menu);
     if (!targetElement.classList.contains('dropdown-item')) {
       if (menu.classList.contains('show')) {
         menu.classList.remove('show');
@@ -20,7 +18,6 @@ export class DropdownDirective {
       }
 
     }
-    // this.element.nativeElement.nextElementSibling.classList.toggle('show');
   }
   constructor(private element: ElementRef) { }
 
