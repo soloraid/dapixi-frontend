20e10e0a246af655d4ae4f3800d44043bd551d2a
ali <aliqbhastam@gmail.com>
2021-01-04 10:31:08 +0330

    clean app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,107 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
-import { EnterComponent } from './auth/enter/enter.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileDetailComponent } from './profile/profile-detail/profile-detail.component';
-import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
-import { ProfileComponent } from './profile/profile.component';
-import { UploadPostComponent } from './upload-post/upload-post.component';
-import { PostDetailComponent } from './post-detail/post-detail.component';
-import { SearchResultComponent } from './search/search-result/search-result.component';
-import { AuthGuard } from './share/auth.guard';
-import { ReAuthGuard } from './share/re-auth.guard';
-import { ResetComponent } from './auth/reset/reset.component';
 import { Error404Component } from './error404/error404.component';
 import { Error500Component } from './error500/error500.component';
-import { MoreComponent } from './more/more.component';
-import { ConfirmComponent } from './auth/confirm/confirm.component';
-import { InfoComponent } from './info/info.component';
-import { CommentComponent } from './comment/comment.component';
-import { MainComponent } from './main/main.component';
-import { FollowPostsComponent } from './follow-posts/follow-posts.component';
-import { RecomendedPostsComponent } from './recomended-posts/recomended-posts.component';
-import { HotPostsComponent } from './hot-posts/hot-posts.component';
-import { ThirdPartyOAuthSSOService } from './third-party-oauth-sso.service';
-import {FollowComponent} from './profile/follow/follow.component';
 
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   // pathMatch: 'full',
-  //   component: MainComponent,
-  //   resolve: { tokens: ThirdPartyOAuthSSOService },
-  //   children: [
-  //     {
-  //       path: '',
-  //       pathMatch: 'full',
-  //       // component: MainComponent
-  //       redirectTo: 'home'
-  //     },
-  //     {
-  //       path: 'home',
-  //       component: HomeComponent
-  //     },
-  //     {
-  //       path: 'follow',
-  //       component: FollowPostsComponent,
-  //       canActivate: [AuthGuard]
-  //     },
-  //     {
-  //       path: 'recommend',
-  //       component: RecomendedPostsComponent,
-  //       canActivate: [AuthGuard]
-  //     },
-  //     {
-  //       path: 'hot',
-  //       component: HotPostsComponent,
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: 'home',
-  //   component: HomeComponent,
-  // },
-  {
-    path: 'comment',
-    component: CommentComponent,
-  },
-  {
-    path: 'more/:type',
-    component: MoreComponent
-  },
-  // {
-  //   path: 'info',
-  //   component: InfoComponent
-  // },
-  // {
-  //   path: 'post-detail/:id',
-  //   component: PostDetailComponent
-  // },
-  // {
-  //   path: 'search-result',
-  //   // component: SearchResultComponent,
-  //   children: [
-  //     {
-  //       path: 'username/:name',
-  //       component: SearchResultComponent,
-  //       data: { name: 'username' }
-  //     },
-  //     {
-  //       path: 'category',
-  //       component: SearchResultComponent,
-  //       data: { name: 'category' }
-  //     },
-  //     {
-  //       path: 'title/:title',
-  //       component: SearchResultComponent,
-  //       data: { name: 'title' }
-  //     },
-  //   ]
-  // },
-
   {
     path: '404',
     component: Error404Component
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,67 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-
-import { ProfileComponent } from './profile/profile.component';
-import { ProfileDetailComponent } from './profile/profile-detail/profile-detail.component';
-import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
-import { PostDetailComponent } from './post-detail/post-detail.component';
-import { PostComponent } from './share/post/post.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { UploadPostComponent } from './upload-post/upload-post.component';
-import { DropdownDirective } from './share/dropdown.directive';
-import { ClipboardModule } from 'ngx-clipboard';
+import { FormsModule} from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorInterceptor } from './auth/auth-interceptor.interceptor';
-import { SearchComponent } from './search/search.component';
-import { SearchResultComponent } from './search/search-result/search-result.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-
-
+import { SearchComponent } from './search/search.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './header/main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 import { Error404Component } from './error404/error404.component';
 import { Error500Component } from './error500/error500.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { InterceptorService } from './share/loader/interceptor.service';
-import { MoreComponent } from './more/more.component';
 
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatTabsModule } from '@angular/material/tabs';
-
-import { ConfirmComponent } from './auth/confirm/confirm.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { InfoComponent } from './info/info.component';
-import { CommentComponent } from './comment/comment.component';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatStepperModule } from '@angular/material/stepper';
-import { NgxMasonryModule } from 'ngx-masonry';
-import { MainComponent } from './main/main.component';
-import { FollowPostsComponent } from './follow-posts/follow-posts.component';
-import { RecomendedPostsComponent } from './recomended-posts/recomended-posts.component';
-import { HotPostsComponent } from './hot-posts/hot-posts.component';
-import { MasonryPostsComponent } from './share/masonry-posts/masonry-posts.component';
 import { CatchErrorInterceptor } from './catch-error.interceptor';
 import { MatMenuModule } from '@angular/material/menu';
-import { FollowComponent } from './profile/follow/follow.component';
-import { ConfirmDialogComponent } from './share/confirm-dialog/confirm-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
@@ -79,54 +40,30 @@ import { InfoModule } from './info/info.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    // ConfirmDialogComponent,
-    // PostDetailComponent,
-    // PostComponent,
-    // UploadPostComponent,
-    // DropdownDirective,
     SearchComponent,
-    // SearchResultComponent,
     MainNavComponent,
     Error404Component,
     Error500Component,
-    MoreComponent,
-    // InfoComponent,
-    // CommentComponent,
-    // MainComponent,
-    // FollowPostsComponent,
-    // RecomendedPostsComponent,
-    // HotPostsComponent,
-    // MasonryPostsComponent,
-    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    ClipboardModule,
+
     HttpClientModule,
-    NgxPaginationModule,
+
     BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
-    MatButtonModule,
+
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatProgressSpinnerModule,
-    MatProgressBarModule,
-    MatTabsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatExpansionModule,
-    MatCardModule,
-    MatTableModule,
+
     MatTooltipModule,
-    MatStepperModule,
-    NgxMasonryModule,
+
     MatMenuModule,
-    MatDialogModule,
+
     MatAutocompleteModule,
     AuthModule,
     MasonryPostsModule,
@@ -153,9 +90,6 @@ import { InfoModule } from './info/info.module';
       multi: true
     }
   ],
-  // exports:[
-  //   DropdownDirective
-  // ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
