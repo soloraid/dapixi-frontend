0c0c42f5b17a3a3dc370bf8bcf7ad24a6800f20e
hashem <hm.praw@yahoo.com>
2021-01-21 05:24:19 +0330

    create pipe module for using in all project

---
+++ src/app/share/pipes/pipe.module.ts
@@ -0,0 +1,9 @@
+import {DateAgoPipe} from './date-ago.pipe';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [],
+  declarations: [DateAgoPipe],
+  exports: [DateAgoPipe]
+})
+export class PipeModule { }
